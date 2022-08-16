3dfeec1bd0dc1b261fadf8d66854a2e2021361d3
Sameer <sameer_ahmed141@hotmail.com>
2022-08-16 11:27:05 +0500

    Add functionality to modal and get inputs

--- app.js
+++ app.js
@@ -1,60 +1,111 @@
-//Setting the gameboard module 
-const gameBoard = (() => {
-    let boxes = document.querySelectorAll('.boxes');
-    let gameboard = ['o','x','o','x','o','x','o','x','o'];
-    return{gameboard};
-})();
-
-//Setting the display controller module
-const displayController = (() => {
-
-   let boxes = document.querySelectorAll('.boxes');
-   let marks = gameBoard.gameboard;
-   let index = 0;
-   //function to display the mark to the webpage
-   const displayMark = (mark,box) => {
-    if(box.textContent === ''){
-        box.textContent = mark;
-        index++;
-    }
-   };
-
-   //Initialize the board
-   const displayBoard = () => {
-    boxes.forEach(box => {
-        box.addEventListener('click',(e)=>{
-    
-            displayMark(marks[index],e.target);
-
-        });
-        });
-   };
-
-   //reset board
-   const resetBoard = () => {
-    boxes.forEach(box => {
-        box.textContent = '';
-    });
-   }
-
-   return{displayBoard,resetBoard};
-   
-})();
-
-
-
-//Creating the game object
-const game = (() => {
-    
-})();
-
-//Setting factory function for players
-const players = (playerName,playerMark) => {
-    return {playerName,playerMark};
+//Creating Player Objects
+const Player = (name,markSelected) => {
+    return {name,markSelected};
 };
 
-let player1 = players('Jim','X');
-let player2 = players('James','o');
-displayController.displayBoard();
- 
-//Add function to get user inputs. Populate the gameboard array accordingly
+//Setting up the Game Board module
+const gameBoard = (() => {
+    const board = ['','','','','','','','',''];
+
+    //push the input into array 
+    const setMark = (mark,index) => {
+        
+    };
+
+    //Get the input
+    const getMark = (index) => {
+
+    };
+
+    //reset the board
+    const reset = () => {
+        for (let i = 0; i<board.length; i++){
+            board[i] = '';
+        }
+    };
+    return{board,setMark,getMark,reset};
+})();
+
+//Setting Display Controller Module
+
+const displayController = (() => {
+    const addPlayer = document.querySelector('.addPlayer');
+    const modal = document.querySelector('.modal');
+    const message = document.querySelector('.message');
+    const boxes = document.querySelectorAll('.boxes');
+    const mark = document.querySelectorAll('.mark');
+    const start = document.querySelector('.start')
+
+    addPlayer.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
+
+    //clicking anywhere to close the modal
+    window.onclick = function(event) {
+        if (event.target == modal || event.target == start) {
+          modal.style.display = "none";
+        }
+      }
+
+    //Create a toggle function for selected mark
+    mark.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            let player = e.target.parentElement;
+            let current = player.getElementsByClassName("active");
+             if (current.length > 0) { 
+                current[0].className = current[0].className.replace(" active", "");
+            }
+            e.target.className += " active";
+        });
+    }); 
+
+   //add functionality to boxes 
+    const displayMark = () => {
+        boxes.forEach(box => {
+            box.addEventListener('click', (e) => {
+               gameController.playRound(e.target.dataset.index);
+            });
+        });
+    };
+
+    const updateBoard = () => {
+
+    };
+
+    return {displayMark};
+})();
+
+// Setting the game controller module
+
+const gameController =  (()=> {
+    const start = document.querySelector('.start');
+    const name1 = document.querySelector('#name1');
+    const name2 = document.querySelector('#name2');
+    const marks = document.querySelectorAll('.mark');
+    let round = 1;
+
+    start.addEventListener('click',() => {
+        let players = setInputs();
+        displayController.displayMark();
+    } );
+
+    const setInputs = () => {
+       let selectedMark = []; 
+       marks.forEach(mark => {
+        if(mark.className.includes("active")){
+            selectedMark.push(mark.value);
+        }
+       });
+       const Player1 = Player(name1.value,selectedMark[0]);
+       const Player2 = Player(name2.value,selectedMark[1]);
+       return {Player1,Player2};
+    }; 
+
+    const playRound = (fieldset) => {
+        console.log(fieldset);
+    };
+
+    const currentPlayer = () => {};
+
+    return {playRound};
+})();
