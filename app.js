e43f3c4df9e197b1ea78621e38db9c4fce2e1292
Sameer <sameer_ahmed141@hotmail.com>
2022-08-24 18:16:15 +0500

    Add function to check winner
    Problem:
    1. array chaining was not working as expected.
    2. Array chaining is noy solved

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Creating Player Objects
-const Player = (name,markSelected) => {
-    return {name,markSelected};
+const Player = (name,mark) => {
+
+    return {name,mark};
 };
 
 //Setting up the Game Board module
@@ -9,12 +10,15 @@ const gameBoard = (() => {
 
     //push the input into array 
     const setMark = (mark,index) => {
-        
+        if(board.length <= 9){
+            board[index] = mark; 
+        }
     };
 
     //Get the input
     const getMark = (index) => {
-
+        if(index>board.length) return;
+        return board[index];
     };
 
     //reset the board
@@ -34,7 +38,8 @@ const displayController = (() => {
     const message = document.querySelector('.message');
     const boxes = document.querySelectorAll('.boxes');
     const mark = document.querySelectorAll('.mark');
-    const start = document.querySelector('.start')
+    const start = document.querySelector('.start');
+    const inputs = document.querySelectorAll('input');
 
     addPlayer.addEventListener('click', () => {
         modal.style.display = 'block';
@@ -44,6 +49,7 @@ const displayController = (() => {
     window.onclick = function(event) {
         if (event.target == modal || event.target == start) {
           modal.style.display = "none";
+          clearModal();
         }
       }
 
@@ -63,16 +69,34 @@ const displayController = (() => {
     const displayMark = () => {
         boxes.forEach(box => {
             box.addEventListener('click', (e) => {
-               gameController.playRound(e.target.dataset.index);
+                if (gameController.isOver || e.target.textContent !== "") return;
+                gameController.playRound(parseInt(e.target.dataset.index));
+                updateBoard(e.target);
             });
         });
     };
-
-    const updateBoard = () => {
-
+    //Displaying message
+    const messageDisplay = (msg) => {
+       message.textContent = msg;
     };
 
-    return {displayMark};
+    const updateBoard = (box) => {
+       box.textContent = gameBoard.getMark(box.dataset.index); 
+    };
+
+    //Clearing Modal on start 
+    const clearModal = () => {  
+        inputs.forEach(entry => {
+            entry.value ='';
+        });
+        mark.forEach(mark => {
+            if (mark.className.includes('active')){
+                mark.classList.remove('active');
+            }
+        });
+    };
+
+    return {displayMark,messageDisplay,clearModal,updateBoard};
 })();
 
 // Setting the game controller module
@@ -83,10 +107,17 @@ const gameController =  (()=> {
     const name2 = document.querySelector('#name2');
     const marks = document.querySelectorAll('.mark');
     let round = 1;
+    let isOver = false;
+    let Player1,
+        Player2;
 
     start.addEventListener('click',() => {
-        let players = setInputs();
+        setInputs();
+        randomFirstPlayer();
         displayController.displayMark();
+        displayController.clearModal();
+        displayController.messageDisplay(`${currentPlayer()['name']}'s turn`);
+
     } );
 
     const setInputs = () => {
@@ -96,16 +127,61 @@ const gameController =  (()=> {
             selectedMark.push(mark.value);
         }
        });
-       const Player1 = Player(name1.value,selectedMark[0]);
-       const Player2 = Player(name2.value,selectedMark[1]);
+        Player1 = Player(name1.value,selectedMark[0]);
+        Player2 = Player(name2.value,selectedMark[1]);
        return {Player1,Player2};
     }; 
 
     const playRound = (fieldset) => {
-        console.log(fieldset);
+        gameBoard.setMark(currentPlayer()['mark'],fieldset);
+        if(winCheck()){
+                displayController.messageDisplay(`${currentPlayer()['name']} has won`);
+                isOver=true;
+                return;
+            }
+        if(round === 9){
+                displayController.messageDisplay('Its a Draw');
+                isOver =true;
+                return;
+            }
+
+        round++;
+        displayController.messageDisplay(`${currentPlayer()['name']}'s turn`);
+        console.log(gameBoard.board);
+       
     };
 
-    const currentPlayer = () => {};
+    const currentPlayer = () => {
+        
+        return (round%2 === 1) ? Player1:Player2;
+    };
+   
+    const randomFirstPlayer = () =>{
+        let randomNumber = Math.floor(Math.random()*2) + 1;
+        if (randomNumber === 2){
+            let swap = function(x){return x};
+            Player2 = swap(Player1,Player1=Player2);
+        }
+    };
 
-    return {playRound};
+    const winCheck = () => {
+        const winConditions = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6],
+        ];
+        return winConditions
+        .some((combination) => 
+            combination.every(
+                (index) => gameBoard.getMark(index) === currentPlayer()['mark']
+            )
+        );
+    };
+
+    return {playRound,currentPlayer,Player1,Player2,isOver};
 })();
